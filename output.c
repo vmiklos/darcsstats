12a4aa148109bc0fb2af761a3df99066448395dc
VMiklos <vmiklos@frugalware.org>
2006-01-01 12:08:54 +0000

    2006: happy new year to everyone!

--- output.c
+++ output.c
@@ -1,7 +1,7 @@
 /*
  *  output.c
  * 
- *  Copyright (c) 2005 by Miklos Vajna <vmiklos@frugalware.org>
+ *  Copyright (c) 2005-2006 by Miklos Vajna <vmiklos@frugalware.org>
  * 
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
